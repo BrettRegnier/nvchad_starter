662da8940c8bc8cd1f3a52253bcd2af32b4c1e61
Brett Regnier <bretternestregnier@gmail.com>
2025-07-26 14:44:09 -0600

    Add custom mapping for telescope

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,5 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-
+map("n", "<C-p>", "<cmd> Telescope find_files <CR>")
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
